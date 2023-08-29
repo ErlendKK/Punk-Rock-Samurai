dbf4733d3413636a35efb9ef8414186654cb9ccd
ErlendKK <136587757+ErlendKK@users.noreply.github.com>
2023-08-29 10:32:17 +0200

    Update config.js
    Major game update

--- config.js
+++ config.js
@@ -1,12 +1,21 @@
+/*____              _      ____            _      ____                                  _ 
+ |  _ \ _   _ _ __ | | __ |  _ \ ___   ___| | __ / ___|  __ _ _ __ ___  _   _ _ __ __ _(_)
+ | |_) | | | | '_ \| |/ / | |_) / _ \ / __| |/ / \___ \ / _` | '_ ` _ \| | | | '__/ _` | |
+ |  __/| |_| | | | |   <  |  _ < (_) | (__|   <   ___) | (_| | | | | | | |_| | | | (_| | |
+ |_|    \__,_|_| |_|_|\_\ |_| \_\___/ \___|_|\_\ |____/ \__,_|_| |_| |_|\__,_|_|  \__,_|_|
+
+Game design and programming: Copyright 2023 Erlend Kulander Kvitrud, all rights reserved.*/
+
 const config = {
     type: Phaser.AUTO,
     width: 1100,
     height: 680,
     scene: [
-        Preload, Mainmenu,
-        Level1fight1, Level1fight2, Level1fight3,
-        Level2fight1, Level2fight2, Level2fight3,
-        Level3fight1, Level3fight2, Level3fight3,
+        BaseScene, Preload, Mainmenu,
+        Level1Fight1, Level1Fight2, Level1Fight3,
+        Level2Fight1, Level2Fight2, Level2Fight3,
+        Level3Fight1, Level3Fight2, Level3Fight3,
+        Level4Fight1, Level4Fight2, Level4Fight3,
         Endscene
     ],
     scale: {
@@ -22,17 +31,4 @@ const config = {
 const game = new Phaser.Game(config);
 
 
-/* ---------------------- CREDITS -----------------
-
-Music by xDeviruchi
-Punch sounds by @danielsoundsgood (https://danielsoundsgood.itch.io/free-deadly-kombat-sound-effects)
-Healing sound: "Healing (Ripple)" by Dylan Kelk (https://freesound.org/people/SilverIllusionist/)
-Power up sound by MATRIXXX (https://freesound.org/people/MATRIXXX_/)
-Button Sprites by Ian Eborn.
-Thunder sound by Josh74000MC (https://freesound.org/people/Josh74000MC/)
-Victory music by  (https://pixabay.com/users/pixabay-1/)
-Card image by Avery Ross
-
-https://raw.githack.com/ErlendKK/Punk-Rock-Samurai/main/index.html
-*/
-
+//https://raw.githack.com/ErlendKK/Punk-Rock-Samurai/main/index.html
